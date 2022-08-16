18e3d8332724339c629cbc46040b524267ae4c61
zoetrope <a.ikezoe@gmail.com>
2022-08-16 23:14:57 +0900

    Add tests for sample webhook

---
+++ api/v1/sample_webhook_test.go
@@ -0,0 +1,31 @@
+package v1
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+)
+
+var _ = Describe("Webhook Test", func() {
+	ctx := context.Background()
+
+	It("should deny image that has latest tag", func() {
+		sample := &Sample{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "default",
+				Name:      "invalid-sample",
+			},
+			Spec: SampleSpec{
+				Image:    "nginx:latest",
+				Replicas: pointer.Int32(2),
+			},
+		}
+		err := k8sClient.Create(ctx, sample)
+		Expect(err).Should(HaveOccurred())
+
+		Expect(err.Error()).Should(ContainSubstring("image cannot have latest tag"))
+	})
+})
